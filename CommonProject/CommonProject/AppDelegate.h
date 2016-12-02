94a3ea8eb08613fbef0c1196424f8d897881cd5c
ly <fits@fitsdeMac-mini.local>
2016-12-02 11:58:27 +0800

    上传新建文件
    新建文件

---
+++ CommonProject/CommonProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommonProject
+//
+//  Created by fits on 16/12/2.
+//  Copyright © 2016年 fits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CommonProject/CommonProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CommonProject
+//
+//  Created by fits on 16/12/2.
+//  Copyright © 2016年 fits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
